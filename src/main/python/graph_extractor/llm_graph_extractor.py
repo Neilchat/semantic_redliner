99a3e916704abb8b96362b887b9343bd73d91cc6
saswata <saswatach@gmail.com>
2025-03-05 21:49:22 +0000

    Sectionwise Strategy

--- src/main/python/graph_extractor/llm_graph_extractor.py
+++ src/main/python/graph_extractor/llm_graph_extractor.py
@@ -112,29 +112,3 @@ Please help me decompose the following piece of text into a graph using the stru
                 file.write(json_data)
 
         return graph
-
-
-if __name__ == "__main__":
-    if __name__ == "__main__":
-        config = StrategyConfig(
-            "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",
-            "https://api.openai.com",
-            "",
-            "gpt-4o-mini",
-            "")
-        ge = GraphExtractor(config)
-        tika_parser = TikaParser()
-
-        text = tika_parser.get_text("/Users/saswata/Documents/semantic_redliner/src/main/python/data/Jan 2015.docx")
-        graph = ge.get_or_create_graph(text, "2015")
-
-        pattern = ""
-        for section in graph.sections:
-            pattern = pattern + section.heading + "<.*?>"
-        match = re.search(pattern, text)
-        print(match)
-
-
-
-        # text = tika_parser.get_text("/Users/saswata/Documents/semantic_redliner/src/main/python/data/Mar 2023.docx")
-        # ge.get_or_create_graph(text, "2023")
